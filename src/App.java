284a6db6a9e0187c5e7ee883697e8ee466015926
Kakuk Levente <kakuklevente1@gmail.com>
2023-02-23 15:11:16 +0100

    valami

---
+++ src/App.java
@@ -0,0 +1,11 @@
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        //Névtelen példány jön létre
+        new Tombsug();        
+    }
+
+    public static double calcRadius(int i, int j) {
+        return 0;
+    }
+}
